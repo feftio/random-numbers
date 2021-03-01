14b1161c22c2fd120136ded0318c4eaf9fd4c955
Lik Eduard <feft99@gmail.com>
2021-03-02 05:13:08 +0600

    Poisson alg

--- algorithms/PoissonDist.py
+++ algorithms/PoissonDist.py
@@ -1,9 +1,18 @@
-def _PoissonDist():
-    pass
+from math import e, factorial as fac
+
+
+def _PoissonDist(p, n):
+    sequence, l = [], n * p
+    for m in range(n):
+        sequence.append(((l ** m) * (e ** (-l))) / fac(m))
+    return sequence
+
 
 def PoissonDist():
     pass
 
+
 if __name__ == '__main__':
-    p = 0.2   # Вероятность
-    n = 10    # Количесвто событий
+    p = 0.08   # Вероятность
+    n = 100    # Количесвто событий
+    print(_PoissonDist(p, n))
