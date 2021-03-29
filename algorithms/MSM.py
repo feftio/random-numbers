6d595c0c17bb0279776fc33f90d43761f779e72b
Lik Eduard <feft99@gmail.com>
2021-03-29 23:42:06 +0600

    rename MSM params

--- algorithms/MSM.py
+++ algorithms/MSM.py
@@ -1,32 +1,32 @@
 from utils import digits_up, digits_down
 
 
-def _MSM(number, count):
+def _MSM(z, n):
     sequence = []
-    for _ in range(count):
-        (number, counter) = digits_up(number) if (
-            number > -1 and number < 1) else (number, len(str(number)))
-        number = int(number ** 2 %
+    for _ in range(n):
+        (z, counter) = digits_up(z) if (
+            z > -1 and z < 1) else (z, len(str(z)))
+        z = int(z ** 2 %
                      (10 ** (counter + counter // 2)) / (10 ** (counter // 2)))
-        sequence.append(digits_down(number)[0])
+        sequence.append(digits_down(z)[0])
     return sequence
 
-    # number = int(number % (10 ** 6) / (10 ** 2)) # 4 counter = 7
-    # number = int(number % (10 ** 7) / (10 ** 2)) # 5 counter = 9
-    # number = int(number % (10 ** 9) / (10 ** 3)) # 6 counter = 11
-    # number = int(number % (10 ** 10) / (10 ** 3)) # 7 counter = 13
-    # number = int(number % (10 ** 12) / (10 ** 4)) # 8 counter = 15
+    # z = int(z % (10 ** 6) / (10 ** 2)) # 4 counter = 7
+    # z = int(z % (10 ** 7) / (10 ** 2)) # 5 counter = 9
+    # z = int(z % (10 ** 9) / (10 ** 3)) # 6 counter = 11
+    # z = int(z % (10 ** 10) / (10 ** 3)) # 7 counter = 13
+    # z = int(z % (10 ** 12) / (10 ** 4)) # 8 counter = 15
 
 
 def MSM(cli, name):
     cli.out(f'Вы выбрали [magenta]{name}[/magenta].')
-    number = cli.float('Введите начальное значение (z0): ')
-    count = cli.int('Введите количество генерируемых чисел (n): ')
-    cli.table(['Число'], _MSM(number, count),
+    z0 = cli.float('Введите начальное значение (z0): ')
+    n = cli.int('Введите количество генерируемых чисел (n): ')
+    cli.table(['Число'], _MSM(z0, n),
               autoheader='z(i)', autoformat='z({})')
 
 
 if __name__ == "__main__":
-    number = 0.8933   # Начальное значение
-    count = 3         # Количество генерируемых чисел
-    print(_MSM(number, count))
+    z0 = 0.8933   # Начальное значение
+    n = 3         # Количество генерируемых чисел
+    print(_MSM(z0, n))
