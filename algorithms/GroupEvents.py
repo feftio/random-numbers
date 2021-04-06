c8e5232962f60a11067eb8f7562a07b12d2be249
Lik Eduard <feft99@gmail.com>
2021-04-06 15:51:41 +0600

    GroupEvents.py completed

--- algorithms/GroupEvents.py
+++ algorithms/GroupEvents.py
@@ -1,32 +1,36 @@
-from LCG import _LCG
 from random import random
-from utils import prevsum, normalize, freqconvert
-from decimal import Decimal
+from utils import prevsum
 
 
-def _GroupEvents(probs, count):
-    if (Decimal(str(sum(probs))) != 1):
-        raise Exception('Сумма значений вероятностей не равна 1.')
-    sequence = [random() for _ in range(count)]
-    probs = prevsum(probs)
-    frequency = [0] * (len(probs))
+def _GroupEvents(p, n):
+    # round(float(Decimal(str(sum(p)))), 2)
+    if (round(sum(p), 3) != 1):
+        return 'Сумма вероятностей не равна 1.'
+    sequence = [random() for _ in range(n)]
+    p = prevsum(p)
+    frequency = [0] * (len(p))
     for number in sequence:
-        for i in range(len(probs)):
-            if number <= probs[i]:
+        for i in range(len(p)):
+            if number <= p[i]:
                 frequency[i] += 1
                 break
     return frequency
 
 
-def GroupEvents():
-    print('Вы выбрали моделирование группы событий.')
-    probs = [float(prob) for prob in input(
-        'Введите через пробел набор вероятностей событий (probs): ').split()]
-    count = int(input('Введите количество событий (count): '))
-    print(_GroupEvents(probs, count))
+def GroupEvents(cli, name):
+    cli.out(f'Вы выбрали [magenta]{name}[/magenta].')
+    p = cli.float_list(
+        'Введите через пробел набор вероятностей событий (p1 p2 ...): ')
+    n = cli.int('Введите количество событий (n): ')
+    frequency = _GroupEvents(p, n)
+    if isinstance(frequency, str):
+        cli.out(frequency)
+    else:
+        headers = [f'Событие {i + 1}' for i in range(len(frequency))]
+        cli.table(headers, [frequency])
 
 
 if __name__ == "__main__":
-    probs = [0.1, 0.2, 0.3, 0.4]   # Набор вероятностей событий
-    count = 10                     # Количество событий
-    print(_GroupEvents(probs, count))
+    p = [0.1, 0.2, 0.3, 0.4]   # Набор вероятностей событий
+    n = 10                     # Количество событий
+    print(_GroupEvents(p, n))
