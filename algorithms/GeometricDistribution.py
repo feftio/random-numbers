f8fe01688c7a62a98deeea6255b38aa7eb36c48b
Lik Eduard <feft99@gmail.com>
2021-04-06 15:06:27 +0600

    GeometricDistribution.py completed

--- algorithms/GeometricDistribution.py
+++ algorithms/GeometricDistribution.py
@@ -1,12 +1,16 @@
 def _GeometricDistribution(p, n):
     sequence, q = [], 1 - p
     for i in range(1, n + 1):
-        sequence.append(q ** (i - 1) * p)
+        sequence.append(float("{0:.16f}".format(q ** (i - 1) * p)))
     return sequence
 
 
 def GeometricDistribution(cli, name):
-    pass
+    cli.out(f'Вы выбрали [magenta]{name}[/magenta].')
+    p = cli.float('Введите значение вероятности: ')
+    n = cli.int('Введите количество событий: ')
+    cli.table(['Число'], _GeometricDistribution(p, n),
+              autoheader='z(i)', autoformat='z({})')
 
 
 if __name__ == '__main__':
