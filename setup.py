295990c73e3469ae523aeed5453af7d12df6cc05
Lik Eduard <feft99@gmail.com>
2021-04-15 17:23:01 +0600

    setup: added url and formatted

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
- 
+
 setup(
-  name='random-numbers',
-  description='This is the package about using random numbers.',
-  url='',  
-  author='Lik Eduard',
-  author_email='feft99@gmail.com',
-  license='MIT', 
-  keywords='random', 
-  packages=find_packages()
+    name='random-numbers',
+    description='This is the package about using random numbers.',
+    url='https://github.com/feftio/random-numbers',
+    author='Lik Eduard',
+    author_email='feft99@gmail.com',
+    license='MIT',
+    keywords='random',
+    packages=find_packages()
 )
